d17b707c3f9cd9c41dc1532b7c902b08fdbce93a
QuynhNhi <quynhnhi927@gmail.com>
2024-02-24 15:35:05 +0700

    Begin TestNG

---
+++ src/test/java/_5TestNGFramework_Demo/LoginTest.java
@@ -0,0 +1,42 @@
+package _5TestNGFramework_Demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LoginTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void createDriver() {
+        //WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+    }
+
+    @Test
+    public void testAnhTesterBlog() throws InterruptedException {
+        driver.get("https://anhtester.com");
+        driver.findElement(By.xpath("//a[normalize-space()='tools']")).click();
+        Thread.sleep(2000);
+    }
+    @Test
+    public void testGoogleSearch2() throws InterruptedException {
+        driver.get("https://www.google.com/");
+        driver.findElement(By.xpath("//textarea[@id='APjFqb']")).sendKeys("anhtester", Keys.ENTER);
+        Thread.sleep(1000);
+    }
+
+    @AfterMethod
+    public void closeDriver() {
+        driver.quit();
+    }
+}
